c1bab07f5321678868a88bc2d2c527755c64d393
Md Ridwan Islam <19-39432-1@student.aiub.edu>
2021-03-24 16:53:33 +0600

    Lab Task 5

---
+++ Lab Task 5/Lab Task 5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a1 = new Account("Rijon", "1122", 5000);
+            var a2 = new Account("Ridwan", "3344", 4000);
+            a1.Withdraw(500);
+            a1.Deposit(1000);
+            a1.Transfer(1000, a2);
+            a1.showAllTransactions();
+            a2.showAllTransactions();
+        }
+    }
+}
