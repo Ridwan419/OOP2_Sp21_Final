78d42e1fdb4adaa2ad5f5d3a5a5dd638a290b07f
Md Ridwan Islam <19-39432-1@student.aiub.edu>
2021-04-19 02:52:58 +0600

    DB GUI

---
+++ DB GUI/Database.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DB_GUI
+{
+    class Database
+    {
+        public static SqlConnection ConnectDB()
+        {
+            string connString = @"Server =LAPTOP-D1FAH94M\SQLEXPRESS; Database =demo; Integrated Security = true;";
+            SqlConnection conn = new SqlConnection(connString);
+            return conn;
+        }
+    }
+}
---
+++ DB GUI/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace DB_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbCName = new System.Windows.Forms.TextBox();
+            this.tbCCode = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.dtCourses = new System.Windows.Forms.DataGridView();
+            this.loadData = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbSrchCId = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.tbCCodeUpdate = new System.Windows.Forms.TextBox();
+            this.tbCnameUpdate = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtCourses)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Course Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Course Code:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // tbCName
+            // 
+            this.tbCName.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCName.Location = new System.Drawing.Point(210, 26);
+            this.tbCName.Name = "tbCName";
+            this.tbCName.Size = new System.Drawing.Size(208, 39);
+            this.tbCName.TabIndex = 2;
+            // 
+            // tbCCode
+            // 
+            this.tbCCode.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCCode.Location = new System.Drawing.Point(210, 71);
+            this.tbCCode.Name = "tbCCode";
+            this.tbCCode.Size = new System.Drawing.Size(208, 39);
+            this.tbCCode.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(210, 125);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(141, 39);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "SUBMIT";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // dtCourses
+            // 
+            this.dtCourses.BackgroundColor = System.Drawing.Color.MintCream;
+            this.dtCourses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtCourses.Location = new System.Drawing.Point(516, 12);
+            this.dtCourses.Name = "dtCourses";
+            this.dtCourses.RowHeadersWidth = 51;
+            this.dtCourses.RowTemplate.Height = 24;
+            this.dtCourses.Size = new System.Drawing.Size(456, 231);
+            this.dtCourses.TabIndex = 5;
+            // 
+            // loadData
+            // 
+            this.loadData.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadData.Location = new System.Drawing.Point(516, 249);
+            this.loadData.Name = "loadData";
+            this.loadData.Size = new System.Drawing.Size(108, 39);
+            this.loadData.TabIndex = 6;
+            this.loadData.Text = "LOAD";
+            this.loadData.UseVisualStyleBackColor = true;
+            this.loadData.Click += new System.EventHandler(this.LoadData_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cambria", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(305, 308);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 28);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Edit Panel";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cambria", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(163, 358);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 28);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Course Id:";
+            // 
+            // tbSrchCId
+            // 
+            this.tbSrchCId.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbSrchCId.Location = new System.Drawing.Point(292, 358);
+            this.tbSrchCId.Name = "tbSrchCId";
+            this.tbSrchCId.Size = new System.Drawing.Size(208, 39);
+            this.tbSrchCId.TabIndex = 9;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(516, 358);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(163, 39);
+            this.btnSearch.TabIndex = 10;
+            this.btnSearch.Text = "SEARCH";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.BtnSearch_Click);
+            // 
+            // tbCCodeUpdate
+            // 
+            this.tbCCodeUpdate.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCCodeUpdate.Location = new System.Drawing.Point(292, 466);
+            this.tbCCodeUpdate.Name = "tbCCodeUpdate";
+            this.tbCCodeUpdate.Size = new System.Drawing.Size(208, 39);
+            this.tbCCodeUpdate.TabIndex = 14;
+            // 
+            // tbCnameUpdate
+            // 
+            this.tbCnameUpdate.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCnameUpdate.Location = new System.Drawing.Point(292, 421);
+            this.tbCnameUpdate.Name = "tbCnameUpdate";
+            this.tbCnameUpdate.Size = new System.Drawing.Size(208, 39);
+            this.tbCnameUpdate.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Cambria", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(122, 464);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(156, 28);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Course Code:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Cambria", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(122, 421);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(164, 28);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Course Name:";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Cambria", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(292, 531);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(158, 39);
+            this.btnUpdate.TabIndex = 15;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(1084, 599);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.tbCCodeUpdate);
+            this.Controls.Add(this.tbCnameUpdate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.tbSrchCId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.loadData);
+            this.Controls.Add(this.dtCourses);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbCCode);
+            this.Controls.Add(this.tbCName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtCourses)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbCName;
+        private System.Windows.Forms.TextBox tbCCode;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView dtCourses;
+        private System.Windows.Forms.Button loadData;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbSrchCId;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox tbCCodeUpdate;
+        private System.Windows.Forms.TextBox tbCnameUpdate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
